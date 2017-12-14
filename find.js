5649d392d92d38ff1aeeecfd338c49c6e45bedbe
Iris Tao <mtao@stuy.edu>
2017-12-14 07:26:10 -0500

    comments

--- find.js
+++ find.js
@@ -6,6 +6,8 @@ var boxWidth = box.offsetWidth;
 //randomize later
 //var targetX = boxWidth / 2;
 //var targetY = boxHeight / 2;
+
+//give random x and y coordinates to the target
 var targetX = Math.random() * boxWidth;
 var targetY = Math.random() * boxHeight;
 
@@ -21,17 +23,23 @@ var distance = function (x0, y0, x1, y1) {
 
 console.log(distance(0,0,3,4));
 
+//
 var findIt = function(e) {
   console.log(e.x + ',' + e.y + ' | ' + targetX + ',' + targetY);
   console.log(distance(e.x,e.y,targetX,targetY));
   
-  
   var maxDist = maxDistance(targetX,targetY);
   
   var currentDistance = distance(e.x,e.y,targetX,targetY);
+
+//generate a color string according to the ratio  between current and maximum distance
   var colorString = 'rgb(' + '0,0,' + Math.floor(256 - (256*currentDistance/maxDist)) + ')';
   console.log(colorString);
+
+//change background color using the generated color string
   box.style.backgroundColor=colorString;
+
+  //if the mouse is within a 10px range of the target, set background color to white and display egg image
 	if (isCloseEnough(e.x,e.y,targetX,targetY,10)){
 		box.style.backgroundColor="white";
 		box.style.backgroundImage="url('http://www.incredibleegg.org/wp-content/themes/incredibleegg/assets/images/facts-left-egg.png')";
@@ -39,27 +47,25 @@ var findIt = function(e) {
 
 };
 
-/*
-your OTHER FXNS
 
-*/
+
 var maxDistance = function(pX,pY){
 	var maxX = 0;
 	var maxY = 0;
-	if (pX < box.offsetWidth/2){
-		maxX = box.offsetWidth;
+	if (pX < boxWidth/2){
+		maxX = boxWidth;
 	}
-	if (pY < box.offsetHeight/2){
-		maxY = box.offsetHeight;
+	if (pY < boxHeight/2){
+		maxY = boxHeight;
 	}
 	return distance(pX,pY,maxX,maxY);
 };
 
+//to see if the cursor is within a 10px range of the target
 var isCloseEnough = function(cursorX,cursorY,targetX,targetY,threshold){
 	return (distance(cursorX,cursorY,targetX,targetY) <= threshold);
 	
 };
-
 
 
 box.addEventListener("mousemove", findIt);
